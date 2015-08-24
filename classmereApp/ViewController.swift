2c66c33d4e023310d53e182285ffe6d704adb141
Brandon <leebran@onid.oregonstate.edu>
2015-08-23 23:26:32 -0700

    Initial Commit

---
+++ classmereApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  classmereApp
+//
+//  Created by Brandon Lee on 8/23/15.
+//  Copyright (c) 2015 Brandon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
