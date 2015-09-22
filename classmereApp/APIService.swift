b79ac5af60bf69dcb3c1bd35007ef34bf90a730a
Brandon <leebran@onid.oregonstate.edu>
2015-09-21 18:01:01 -0700

    Added networking activity indicator and color

--- classmereApp/APIService.swift
+++ classmereApp/APIService.swift
@@ -17,50 +17,62 @@ struct APIService {
     
     static func getAllCourses(completion: (JSON) -> Void) {
         SwiftSpinner.show("Fetching Data...")
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
         Alamofire.request(.GET, "\(baseURL)/courses/")
             .responseJSON {(request, response, result) in
                 switch result {
                 case .Success(let data):
                     completion(JSON(data))
                     SwiftSpinner.hide()
-                    
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
                 case .Failure(_, let error):
                     print("Request failed with error: \(error)")
                     SwiftSpinner.hide()
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                 }
         }
     }
     
     static func getCourseByAbbr(abbr: String, completion: (JSON) -> Void) {
         SwiftSpinner.show("Fetching Data...")
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         let encodedAbbr: String = "\(abbr)".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        
         Alamofire.request(.GET, "\(baseURL)/courses/\(encodedAbbr)")
             .responseJSON {(request, response, result) in
                 switch result {
                 case .Success(let data):
                     completion(JSON(data))
                     SwiftSpinner.hide()
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                     
                 case .Failure( _, let error):
                     print("Request failed with error: \(error)")
                     SwiftSpinner.hide()
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                 }
         }
     }
     
     static func searchCourse(searchQuery: String, completion: (JSON) -> Void) {
         SwiftSpinner.show("Fetching Data...")
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         let encodedSearchQuery: String = "\(searchQuery)".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        
         Alamofire.request(.GET, "\(baseURL)/search/courses/\(encodedSearchQuery)")
             .responseJSON {(request, response, result) in
                 switch result {
                 case .Success(let data):
                     completion(JSON(data))
                     SwiftSpinner.hide()
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                     
                 case .Failure(_, let error):
                     print("Request failed with error: \(error)")
                     SwiftSpinner.hide()
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                 }
         }
     }
