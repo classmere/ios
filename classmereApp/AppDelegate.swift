c3ea43a3f09e861bca855bd27937dd9aa6cf25aa
Brandon <leebran@onid.oregonstate.edu>
2015-09-30 02:43:15 -0700

    Enabled Google Maps with API key

--- classmereApp/AppDelegate.swift
+++ classmereApp/AppDelegate.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("")
         return true
     }
 
