214fc3f2827bba48bf0c685d4f2cb77649c91e27
Brandon <leebran@onid.oregonstate.edu>
2015-10-21 20:14:42 -0700

    Added getLocationByAbbr() function

--- classmereApp/APIService.swift
+++ classmereApp/APIService.swift
@@ -56,6 +56,26 @@ struct APIService {
         }
     }
     
+    static func getLocationByAbbr(abbr: String, completion: (JSON) -> Void) {
+        SwiftSpinner.show("Fetching Data...")
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        Alamofire.request(.GET, "\(baseURL)/buildings/\(abbr)")
+            .responseJSON {(request, response, result) in
+                switch result {
+                case .Success(let data):
+                    completion(JSON(data))
+                    SwiftSpinner.hide()
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                    
+                case .Failure(_, let error):
+                    print("Request failed with error: \(error)")
+                    SwiftSpinner.hide()
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                }
+        }
+    }
+    
     static func searchCourse(searchQuery: String, completion: (JSON) -> Void) {
         SwiftSpinner.show("Fetching Data...")
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
