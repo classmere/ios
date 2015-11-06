ce4cc87acdff23512dfb6a3547d99d5945e79b64
Rutger Farry <rutgerfarry@gmail.com>
2015-11-06 00:18:00 -0800

    Add DataFormatter to properly format dates & more

---
+++ classmereApp/DataFormatter.swift
@@ -0,0 +1,44 @@
+//
+//  DataFormatter.swift
+//  classmereApp
+//
+//  Created by Rutger Farry on 11/5/15.
+//  Copyright © 2015 Brandon Lee. All rights reserved.
+//
+
+import Foundation
+
+struct DataFormatter {
+    
+    /**
+        Returns a properly formatted date string from an NSDate
+     
+        - Parameter date: An NSDate optional with a valid date
+        - Returns: A properly formatted date String
+    */
+    static func dateStringFromDate(date: NSDate?) -> String {
+        if let date = date {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateStyle = .ShortStyle
+            return dateFormatter.stringFromDate(date)
+        } else {
+            return ""
+        }
+    }
+    
+    /**
+        Returns a properly formatted time string from an NSDate
+         
+        - Parameter date: An NSDate optional with a valid time
+        - Returns: A properly formatted time String
+     */
+    static func timeStringFromDate(date: NSDate?) -> String {
+        if let date = date {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.timeStyle = .ShortStyle
+            return dateFormatter.stringFromDate(date)
+        } else {
+            return ""
+        }
+    }
+}
