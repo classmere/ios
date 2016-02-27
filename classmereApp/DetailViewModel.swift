de2ddca86541c74358b320d1b7bda432b0a27908
Brandon <leebran@onid.oregonstate.edu>
2016-02-27 02:08:20 -0800

    Add comments for detailviewcontroller/model

--- classmereApp/DetailViewModel.swift
+++ classmereApp/DetailViewModel.swift
@@ -8,15 +8,25 @@
 
 import Foundation
 
+/**
+ Some presentation logic for the today view's detail view.
+ */
 class DetailViewModel {
-
+    
     var course: Course
 
     init(course: Course) {
         self.course = course
     }
-        
     
+    // FIXME: The following method is defined twice (TodayTableViewModel & DetailView Model) need to restructure!
+    /**
+     Calls APIService to get building data for specified sections.
+     
+     - Parameter sections: The sections to get building data for.
+     - Parameter completed: A callback that returns nothing.
+     - Returns: Nothing
+     */
     func fetchBuildingDataForSections(sections: [CourseSection], completed: () -> Void) {
         var buildingsFetched = 0
         for section in sections {
