e6f2b39811bf697c4a8d2a5f7086f3baaeba432b
Brandon <leebran@onid.oregonstate.edu>
2016-02-26 23:52:44 -0800

    Add comments to TodayTableViewCell

--- classmereApp/TodayTableViewCell.swift
+++ classmereApp/TodayTableViewCell.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
+/**
+The main page table view cells upon screen load.
+ */
 class TodayTableViewCell: AbstractClassmereCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var buildingLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
 
+    /**
+     Fill cell with specific course data.
+     
+     - Parameter course: The course object.
+     - Returns: Nothing.
+     */
     func populateWithCourse(course: Course) {
         let section = course.courseSections[0]
         titleLabel.text = course.title?.capitalizedString
