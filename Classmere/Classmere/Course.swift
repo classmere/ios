8e2082aa586ab561084105f0166c91c5fe261ae8
Brandon Lee <brandonlee503@gmail.com>
2016-08-10 19:16:59 -0700

    Implement Course model

--- Classmere/Classmere/Course.swift
+++ Classmere/Classmere/Course.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+/**
+ A model representation of a course at OSU.
+ Reference Docs - https://github.com/classmere/api
+ */
+struct Course {
+    let title: String?
+    let subjectCode: String?
+    let courseNumber: String?
+    let credits: String?
+    let description: String?
+    let abbr: String?
+    
+    var sections = [Section]()
+    
+    init(courseJSON: JSON) {
+        title = courseJSON["title"].string as String?
+        subjectCode = courseJSON["subjectCode"].string as String?
+        courseNumber = courseJSON["courseNumber"].string as String?
+        credits = courseJSON["credits"].string as String?
+        description = courseJSON["description"].string as String?
+        abbr = (subjectCode ?? "") + " " + (courseNumber ?? "0")
+        
+        // If sections exist, create array of sections
+        if let sectionArray = courseJSON["sections"].array {
+            for section in sectionArray {
+                let courseSection = Section(sectionJSON: section)
+                sections.append(courseSection)
+            }
+        }
+    }
+}
