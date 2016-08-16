5d0f8c8dfc3ad0dc12c8a92208720fe84deacb1d
Brandon Lee <brandonlee503@gmail.com>
2016-08-15 18:30:03 -0700

    Add more tests.. might move them to a seperate file later..

--- Classmere/ClassmereTests/APIServiceTests.swift
+++ Classmere/ClassmereTests/APIServiceTests.swift
@@ -49,4 +49,58 @@ class APIServiceTests: XCTestCase {
             XCTAssertNotNil(response)
         })
     }
+    
+    func testCourseModel() {
+        APIService.getCourseBySubjectCode("CS", courseNumber: 161, completion: { response in
+            let course: Course = Course(courseJSON: response)
+            XCTAssertNotNil(course.title)
+            XCTAssertNotNil(course.subjectCode)
+            XCTAssertNotNil(course.courseNumber)
+            XCTAssertNotNil(course.credits)
+            XCTAssertNotNil(course.description)
+            XCTAssertNotNil(course.abbr)
+            XCTAssertNotNil(course.sections)
+        })
+    }
+    
+    func testSectionModel() {
+        APIService.getCourseBySubjectCode("CS", courseNumber: 161, completion: { response in
+            let course: Course = Course(courseJSON: response)
+            let section: Section = course.sections[0]
+            XCTAssertNotNil(section.term)
+            XCTAssertNotNil(section.session)
+            XCTAssertNotNil(section.crn)
+            XCTAssertNotNil(section.credits)
+            XCTAssertNotNil(section.instructor)
+            XCTAssertNotNil(section.startTime)
+            XCTAssertNotNil(section.endTime)
+            XCTAssertNotNil(section.days)
+            XCTAssertNotNil(section.buildingCode)
+            XCTAssertNotNil(section.roomNumber)
+            XCTAssertNotNil(section.startDate)
+            XCTAssertNotNil(section.endDate)
+            XCTAssertNotNil(section.campus)
+            XCTAssertNotNil(section.type)
+            XCTAssertNotNil(section.status)
+            XCTAssertNotNil(section.enrollmentCapacity)
+            XCTAssertNotNil(section.enrollmentCurrent)
+            XCTAssertNotNil(section.waitlistCapacity)
+            XCTAssertNotNil(section.waitlistCurrent)
+            XCTAssertNotNil(section.fees)
+            XCTAssertNotNil(section.restrictions)
+            XCTAssertNotNil(section.comments)
+            XCTAssertNotNil(section.textbookUrl)
+        })
+    }
+    
+    func testBuildingModel() {
+        APIService.getBuildingByAbbr("KEC", completion: { response in
+            let building: Building = Building(buildingJSON: response)
+            XCTAssertNotNil(building.abbr)
+            XCTAssertNotNil(building.address)
+            XCTAssertNotNil(building.buildingNumber)
+            XCTAssertNotNil(building.name)
+            XCTAssertNotNil(building.sqft)
+        })
+    }
 }
