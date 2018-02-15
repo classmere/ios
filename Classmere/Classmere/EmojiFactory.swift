1106b2e41f2f01c3df9bbf46a81013f189f852d1
Rutger Farry <rutgerfarry@gmail.com>
2018-02-14 22:24:32 -0800

    Revert "WIP on Emoji”. Going to leave emoji be for a sec
    This reverts commit 20a319b09c4efdcff2281400036e61fddb945fad.

--- Classmere/Classmere/EmojiFactory.swift
+++ Classmere/Classmere/EmojiFactory.swift
@@ -2,185 +2,300 @@
 
 import Foundation
 
-enum Emoji: String {
-    case YDE = "👶"
-    case ALS = "🎒"
-    case ACTG = "📠"
-    case AHE = "👨"
-    case AS = "🚀"
-    case AED, AGRI, AG = "🌽"
-    case ASL = "✋"
-    case ANS = "🐵"
-    case ANTH = "👴"
-    case AEC = "📉"
-    case ARAB = "🕌"
-    case ART = "🎨"
-    case ASN = "⛩"
-    case ATS = "🌤"
-    case BB = "🌡"
-    case BHS = "😷"
-    case bee, bioe = "🔬"
-    case BI = "🐨"
-    case BRR = "📚"
-    case BOT = "🌷"
-    case BA = "🏢"
-    case CBEE, CHE = "🔬"
-    case CH = "⚗"
-    case CHN = "🇨🇳"
-    case CCE, CE, CEM = "🚧"
-    case COMM = "🗣"
-    case CS = "💻"
-    case CSS, CROP = "🌿"
-    case DHE = "🍥"
-    case ECON = "📈"
-    case ED = "🎓"
-    case ECE = "💡"
-    case EGNR = "🔨"
-    case ENG = "📖"
-    case ENT = "🐛"
-    case EAH = "🌺"
-    case ENVE = "🌲"
-    case ENSC = "🌳"
-    case ES = "👲🏾"
-    case FILM = "📽"
-    case FIN = "💰"
-    case FW = "🐠"
-    case FCSJ = "🍱"
-    case FST = "🍴"
-    case fes, fe, for = "🌲"
-    case FR = "🇫🇷"
-    case GS = "🔬"
-    case GEOG, GEO = "🗺"
-    case GER = "🇩🇪"
-    case GRAD = "🎓"
-    case GD = "🖊"
-    case HHS = "😷"
-    case HEBR = "🇮🇱"
-    case HST, HSTS = "📚"
-    case HC = "🤓"
-    case HORT = "🌻"
-    case HDFS = "👨‍👩‍👧‍👦"
-    case HEST = "👩"
-    case IE = "🛠"
-    case IEPA, IEPG, IEPH = "📖"
-    case INTL = "🌎"
-    case IT = "🇮🇹"
-    case JPN = "🇯🇵"
-    case KIN = "🏃"
-    case LEAD = "👸"
-    case LA, LS = "🙆"
-    case LIB = "📘"
-    case LING = "🖊"
-    case MGMT = "👔"
-    case MFGE = "🛠"
-    case MRKT = "💶"
-    case MATS = "💎"
-    case MTH = "📊"
-    case MIME = "⚒"
-    case ME = "⚙"
-    case MB = "🐤"
-    case MS = "💣"
-    case MUS, MUP, MUED = "🎵"
-    case NR = "💨"
-    case NS = "⚓️"
-    case NMC = "📱"
-    case NSE = "💡"
-    case NUTR = "🍎"
-    case OC = "🌊"
-    case PAX = "☮️"
-    case PHAR = "💊"
-    case PHL = "🤔"
-    case PAC = "⛹"
-    case PH = "💫"
-    case PBG = "🌱"
-    case PS = "🏛"
-    case PSY = "🙇"
-    case H = "🤒"
-    case PPOL = "⚖"
-    case QS = "👬"
-    case RNG = "🏞"
-    case REL = "🕍"
-    case ROB = "🤖"
-    case RS = "🏡"
-    case RUS = "🇷🇺"
-    case SED = "📊"
-    case SOC = "👨‍👩‍👧‍👦"
-    case SOIL = "💩"
-    case SPAN = "🇪🇸"
-    case ST = "📊"
-    case SUS = "🌦"
-    case TA = "🎭"
-    case TOX = "😵"
-    case TCS = "🏙"
-    case UEXP = "🎒"
-    case VMB = "🐶"
-    case WGSS = "👩‍👩‍👧"
-    case WSE = "🌲"
-    case WLC = "🌍"
-    case WR = "📝"
-    case Z = "🐼"
-}
+/**
+ A model representation of an emoji factory.
+ */
+struct EmojiFactory {
 
     /**
-     A model representation of an emoji factory.
+     Returns emoji for a course type
+     - Parameter type: Type of a course
+     - Returns: An emoji string
      */
-    struct EmojiFactory {
-
-        /**
-         Returns emoji for a course type
-         - Parameter type: Type of a course
-         - Returns: An emoji string
-         */
-        static func emojiFromCourseType(_ type: String?) -> String {
-            if let courseType = type {
-                switch courseType {
-                }
-            }
-
-            return "🏫"
-        }
-
-        /**
-         Returns emoji for a section type
-    
-         - Parameter type: Type of a section
-         - Returns: An emoji string
-         */
-        static func emojiFromSectionType(_ type: String?) -> String {
-            if let sectionType = type {
-                switch sectionType {
-                case "Lecture":
-                    return "🗣"
-                case "Laboratory":
-                    return "🔬"
-                case "Recitation":
-                    return "✏️"
-                default:
-                    return "📓"
-                }
-            }
-
-            return "📓"
-        }
-
-        static func emojiFromSectionProperty(_ property: String) -> String {
-            switch property {
-            case "Days":
-                return "☀️"
-            case "Instructor":
-                return "👤"
-            case "Location":
+    static func emojiFromCourseType(_ type: String?) -> String {
+        if let courseType = type {
+            switch courseType {
+            case "YDE":
+                return "👶"
+            case "ALS":
+                return "🎒"
+            case "ACTG":
+                return "📠"
+            case "AHE":
+                return "👨"
+            case "AS":
+                return "🚀"
+            case "AED", "AGRI", "AG":
+                return "🌽"
+            case "ASL":
+                return "✋"
+            case "ANS":
+                return "🐵"
+            case "ANTH":
+                return "👴"
+            case "AEC":
+                return "📉"
+            case "ARAB":
+                return "🕌"
+            case "ART":
+                return "🎨"
+            case "ASN":
+                return "⛩"
+            case "ATS":
+                return "🌤"
+            case "BB":
+                return "🌡"
+            case "BHS":
+                return "😷"
+            case "BEE", "BIOE":
+                return "🔬"
+            case "BI":
+                return "🐨"
+            case "BRR":
+                return "📚"
+            case "BOT":
+                return "🌷"
+            case "BA":
+                return "🏢"
+            case "CBEE", "CHE":
+                return "🔬"
+            case "CH":
+                return "⚗"
+            case "CHN":
+                return "🇨🇳"
+            case "CCE", "CE", "CEM":
+                return "🚧"
+            case "COMM":
+                return "🗣"
+            case "CS":
+                return "💻"
+            case "CSS", "CROP":
+                return "🌿"
+            case "DHE":
+                return "🍥"
+            case "ECON":
+                return "📈"
+            case "ED":
+                return "🎓"
+            case "ECE":
+                return "💡"
+            case "EGNR":
+                return "🔨"
+            case "ENG":
+                return "📖"
+            case "ENT":
+                return "🐛"
+            case "EAH":
+                return "🌺"
+            case "ENVE":
+                return "🌲"
+            case "ENSC":
+                return "🌳"
+            case "ES":
+                return "👲🏾"
+            case "FILM":
+                return "📽"
+            case "FIN":
+                return "💰"
+            case "FW":
+                return "🐠"
+            case "FCSJ":
+                return "🍱"
+            case "FST":
+                return "🍴"
+            case "FES", "FE", "FOR":
+                return "🌲"
+            case "FR":
+                return "🇫🇷"
+            case "GS":
+                return "🔬"
+            case "GEOG", "GEO":
                 return "🗺"
-            case "Type":
-                return "📝"
-            case "Enrolled":
+            case "GER":
+                return "🇩🇪"
+            case "GRAD":
+                return "🎓"
+            case "GD":
+                return "🖊"
+            case "HHS":
+                return "😷"
+            case "HEBR":
+                return "🇮🇱"
+            case "HST", "HSTS":
+                return "📚"
+            case "HC":
+                return "🤓"
+            case "HORT":
+                return "🌻"
+            case "HDFS":
                 return "👨‍👩‍👧‍👦"
-            case "Dates":
-                return "🗓"
-            case "CRN":
-                return "#️⃣"
+            case "HEST":
+                return "👩"
+            case "IE":
+                return "🛠"
+            case "IEPA", "IEPG", "IEPH":
+                return "📖"
+            case "INTL":
+                return "🌎"
+            case "IT":
+                return "🇮🇹"
+            case "JPN":
+                return "🇯🇵"
+            case "KIN":
+                return "🏃"
+            case "LEAD":
+                return "👸"
+            case "LA", "LS":
+                return "🙆"
+            case "LIB":
+                return "📘"
+            case "LING":
+                return "🖊"
+            case "MGMT":
+                return "👔"
+            case "MFGE":
+                return "🛠"
+            case "MRKT":
+                return "💶"
+            case "MATS":
+                return "💎"
+            case "MTH":
+                return "📊"
+            case "MIME":
+                return "⚒"
+            case "ME":
+                return "⚙"
+            case "MB":
+                return "🐤"
+            case "MS":
+                return "💣"
+            case "MUS", "MUP", "MUED":
+                return "🎵"
+            case "NR":
+                return "💨"
+            case "NS":
+                return "⚓️"
+            case "NMC":
+                return "📱"
+            case "NSE":
+                return "💡"
+            case "NUTR":
+                return "🍎"
+            case "OC":
+                return "🌊"
+            case "PAX":
+                return "☮️"
+            case "PHAR":
+                return "💊"
+            case "PHL":
+                return "🤔"
+            case "PAC":
+                return "⛹"
+            case "PH":
+                return "💫"
+            case "PBG":
+                return "🌱"
+            case "PS":
+                return "🏛"
+            case "PSY":
+                return "🙇"
+            case "H":
+                return "🤒"
+            case "PPOL":
+                return "⚖"
+            case "QS":
+                return "👬"
+            case "RNG":
+                return "🏞"
+            case "REL":
+                return "🕍"
+            case "ROB":
+                return "🤖"
+            case "RS":
+                return "🏡"
+            case "RUS":
+                return "🇷🇺"
+            case "SED":
+                return "📊"
+            case "SOC":
+                return "👨‍👩‍👧‍👦"
+            case "SOIL":
+                return "💩"
+            case "SPAN":
+                return "🇪🇸"
+            case "ST":
+                return "📊"
+            case "SUS":
+                return "🌦"
+            case "TA":
+                return "🎭"
+            case "TOX":
+                return "😵"
+            case "TCS":
+                return "🏙"
+            case "UEXP":
+                return "🎒"
+            case "VMB":
+                return "🐶"
+            case "WGSS":
+                return "👩‍👩‍👧"
+            case "WSE":
+                return "🌲"
+            case "WLC":
+                return "🌍"
+            case "WR":
+                return "📝"
+            case "Z":
+                return "🐼"
             default:
                 return "🏫"
             }
         }
+
+        return "🏫"
     }
+
+    /**
+     Returns emoji for a section type
+     
+     - Parameter type: Type of a section
+     - Returns: An emoji string
+     */
+    static func emojiFromSectionType(_ type: String?) -> String {
+        if let sectionType = type {
+            switch sectionType {
+            case "Lecture":
+                return "🗣"
+            case "Laboratory":
+                return "🔬"
+            case "Recitation":
+                return "✏️"
+            default:
+                return "📓"
+            }
+        }
+
+        return "📓"
+    }
+
+    static func emojiFromSectionProperty(_ property: String) -> String {
+        switch property {
+        case "Days":
+            return "☀️"
+        case "Instructor":
+            return "👤"
+        case "Location":
+            return "🗺"
+        case "Type":
+            return "📝"
+        case "Enrolled":
+            return "👨‍👩‍👧‍👦"
+        case "Dates":
+            return "🗓"
+        case "CRN":
+            return "#️⃣"
+        default:
+            return "🏫"
+        }
+    }
+}
