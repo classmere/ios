13409d9460e924f6c4eb635b6aea7da6c00345d1
Rutger Farry <rutgerfarry@gmail.com>
2018-03-01 10:03:33 -0800

    Only show location pin for specific section in SectionVC

--- Classmere/Classmere/SectionViewController.swift
+++ Classmere/Classmere/SectionViewController.swift
@@ -28,52 +28,50 @@ final class SectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let course = course {
+        if let course = course, let section = section {
             title = course.abbr
 
             tableViewDataSource = TableViewDataSource(tableView: tableView)
             tableViewDataSource.rows = [Row<MapCell>(data: [])] +
                 [Row<CourseDetailsCell>(data: course)] +
-                course.sections.map { Row<CourseCell>(data: $0) }
+                [Row<SectionCell>(data: section)]
             tableView.delegate = self
             tableView.dataSource = tableViewDataSource
 
-            fetchBuildings(forCourse: course)
+            fetchBuildings(forCourse: course, section: section)
         } else {
             navigationController!.popViewController(animated: true)
         }
     }
 
     /// Gets all buildings for all meetingTimes for all sections and updates tableView with locations
-    func fetchBuildings(forCourse course: Course) {
+    func fetchBuildings(forCourse course: Course, section: Section) {
         var mapCellPoints = Set<MapCellPoint>()
         var inFlightRequests = 0
 
-        for section in course.sections {
-            let meetingTimes = section.meetingTimes?.filter { $0.buildingCode != nil } ?? []
+        let meetingTimes = section.meetingTimes?.filter { $0.buildingCode != nil } ?? []
 
-            for meetingTime in meetingTimes {
-                guard let code = meetingTime.buildingCode else { return }
-                inFlightRequests += 1
-                store.get(buildingAbbr: code) { result in
-                    inFlightRequests -= 1
-                    switch result {
-                    case .success(let building):
-                        guard let latitude = building.latitude, let longitude = building.longitude else { return }
-                        mapCellPoints.insert(MapCellPoint(buildingName: building.name,
-                                                          buildingCode: building.abbr,
-                                                          roomNumber: meetingTime.roomNumber,
-                                                          latitude: latitude,
-                                                          longitude: longitude,
-                                                          type: section.type))
-                    case .failure(let error):
-                        print("fetchBuildings() error: \(error)")
-                    }
-                    if inFlightRequests == 0 {
-                        self.tableViewDataSource.rows = [Row<MapCell>(data: Array(mapCellPoints))] +
-                            [Row<CourseDetailsCell>(data: course)] +
-                            course.sections.map { Row<CourseCell>(data: $0) }
-                    }
+        for meetingTime in meetingTimes {
+            guard let code = meetingTime.buildingCode else { return }
+            inFlightRequests += 1
+            store.get(buildingAbbr: code) { result in
+                inFlightRequests -= 1
+                switch result {
+                case .success(let building):
+                    guard let latitude = building.latitude, let longitude = building.longitude else { return }
+                    mapCellPoints.insert(MapCellPoint(buildingName: building.name,
+                                                      buildingCode: building.abbr,
+                                                      roomNumber: meetingTime.roomNumber,
+                                                      latitude: latitude,
+                                                      longitude: longitude,
+                                                      type: section.type))
+                case .failure(let error):
+                    print("fetchBuildings() error: \(error)")
+                }
+                if inFlightRequests == 0 {
+                    self.tableViewDataSource.rows = [Row<MapCell>(data: Array(mapCellPoints))] +
+                        [Row<CourseDetailsCell>(data: course)] +
+                        [Row<SectionCell>(data: section)]
                 }
             }
 
