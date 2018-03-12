e1dae8579ba59565880d395b365dc697ee7880e7
Rutger Farry <rutgerfarry@gmail.com>
2018-03-12 16:47:03 -0700

    Fill out SettingsViewController

--- Classmere/Classmere/Controllers/SettingsViewController.swift
+++ Classmere/Classmere/Controllers/SettingsViewController.swift
@@ -2,8 +2,21 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var classmereLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        classmereLabel.attributedText = Theme.classmereLogo(withSize: 17)
+    }
+
+    @IBAction func rutgerfarryButtonPressed(_ sender: Any) {
+        let url = URL(string: "https://twitter.com/rutgerfarry")!
+        UIApplication.shared.open(url, options: [:])
+    }
+
+    @IBAction func brandonlee503ButtonPressed(_ sender: Any) {
+        let url = URL(string: "https://github.com/brandonlee503")!
+        UIApplication.shared.open(url, options: [:])
     }
 
 }
